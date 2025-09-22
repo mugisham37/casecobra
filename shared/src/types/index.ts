9bc22f37e70c20c74658c6a2aabe666f8bdc44b1
mugisham37 <mugisham333@gmail.com>
2025-09-22 09:37:08 +0100

    Removed all of the origin documents from the previous project

--- shared/src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './api.types';
-export * from './auth.types';
-export * from './user.types';
-export * from './product.types';
-export * from './order.types';
-export * from './common.types';
